31e51a1790c05e8456959baef779af5a563a8f8c
Raphael Goeldner <raphael.goeldner@gmail.com>
2019-02-17 03:06:29 +0300

    before checkout

--- app/src/main/java/com/example/rapha/swipeprototype2/api/ApiKey.java
+++ app/src/main/java/com/example/rapha/swipeprototype2/api/ApiKey.java
@@ -2,6 +2,6 @@ package com.example.rapha.swipeprototype2.api;
 
 public class ApiKey {
 
-    public static final String API_KEY_NEWS_API = "42cfadf8db9d46c99c1c9bc9752dfdbe";
+    public static final String API_KEY_NEWS_API = "e904c927ef45417891bda9927c663bea";
     private static final String API_KEY_NEWS_API_OLD = "919137955d4a4543aaef87404795554d";
 }
